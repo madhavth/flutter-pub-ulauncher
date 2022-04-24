07ee6529a05519d10c181d335c595a395fa7e1a8
madhavth <madhavth77@gmail.com>
2022-04-24 11:17:35 +0545

    changed icon

--- Flutterhelper.py
+++ Flutterhelper.py
@@ -1,63 +1,28 @@
-import json
-
 from requests_html import HTMLSession
 
-now_showing = 'now_showing'
-coming_soon = 'coming_soon'
+
+class Package:
+    def __init__(self, package_name, latest_version, description):
+        self.package_name = package_name
+        self.latest_version = latest_version
+        self.description = description
 
 
-class Movie:
-    def __init__(self, movie_name, url, has_ticket, movie_id, showing=False, is3d=None, sold_out=None, reservable=None):
-        self.movie_name = movie_name
-        self.youtube_url = url
-        self.has_ticket = has_ticket
-        self.movie_id = movie_id
-        self.book_url = f"https://www.qfxcinemas.com/show?eventId={movie_id}"
-        self.description = f"3D - {is3d}, Reservable - {reservable}, Sold Out - {sold_out}" if showing else "Tickets available" if self.has_ticket else "No tickets available"
-
-
-def get_coming_soon_movies():
+def search_package(package_name):
     try:
-        s = HTMLSession()
-        r = s.get('https://api.qfxcinemas.com/api/public/ComingSoon')
+        if package_name is None:
+            return []
 
-        response = json.loads(r.html.html)
-        movies = [Movie(movie_name=movie['name'], has_ticket=movie['hasBuyTicket'],
-                        url=movie['mobileTrailerURL'],
-                        movie_id=movie['id'],
-                        )
-                  for movie in response['data']]
-        return movies
+        s = HTMLSession()
+        r = s.get(f'https://pub.dev/packages?q={package_name}')
+        packages_items = r.html.find('.packages-item')
+        items = []
+
+        for package in packages_items:
+            name = package.find('.packages-title', first=True).text
+            items.append(Package(package_name=name, latest_version='',
+                                 description=''))
+        return items
 
     except Exception as e:
         print(e)
-
-
-def get_now_showing_movies():
-    try:
-        s = HTMLSession()
-        r = s.get('https://api.qfxcinemas.com/api/public/NowShowing')
-
-        response = json.loads(r.html.html)
-        movies = [Movie(movie_name=movie['name'], has_ticket=True,
-                        url=movie['mediaLink'],
-                        movie_id=movie['eventID'],
-                        showing=True,
-                        is3d=movie['is3DMovie'],
-                        sold_out=movie['soldOut'],
-                        reservable=movie['isReservable'],
-                        )
-                  for movie in response['data']]
-        return movies
-
-    except Exception as e:
-        print(e)
-
-
-def get_movies(provider):
-    if provider == coming_soon:
-        return get_coming_soon_movies()
-    elif provider == now_showing:
-        return get_now_showing_movies()
-    else:
-        return []
--- main.py
+++ main.py
@@ -4,20 +4,18 @@ import logging
 
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.client.Extension import Extension
-from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
-from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
-from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.event import KeywordQueryEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
-from ulauncher.api.shared.item.ExtensionSmallResultItem import ExtensionSmallResultItem
 
-from Flutterhelper import coming_soon, get_movies, now_showing
+from Flutterhelper import search_package
 
 logger = logging.getLogger(__name__)
 
-QFX_PROVIDERS = sorted([
-    now_showing, coming_soon
+FLUTTER_PUB_PROVIDERS = sorted([
+
 ])
 
 
@@ -28,36 +26,58 @@ class QFXExtension(Extension):
         """ Initializes the extension """
         super(QFXExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
-        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
+        # self.subscribe(ItemEnterEvent, ItemEnterEventListener())
 
-    def list_providers(self, query):
-
-        providers = QFX_PROVIDERS
-        if query:
-            providers = [
-                provider for provider in providers if query.lower() in provider
-            ]
-
-        items = []
-
-        if len(providers) == 0:
+    def search_params(self, query):
+        results = search_package(query)
+        if len(results) == 0:
+            output = f'No packages found by name {query}' if query else 'Enter a package name'
             return RenderResultListAction([
                 ExtensionResultItem(
                     icon='images/icon.png',
-                    name="No provider found matching your criteria",
+                    name=output,
                     highlightable=False,
-                    on_enter=HideWindowAction())
+                    on_enter=HideWindowAction()
+                )
             ])
 
-        for provider in providers[:20]:
+        items = []
+        for result in results[:20]:
             items.append(
-                ExtensionSmallResultItem(
+                ExtensionResultItem(
                     icon='images/icon.png',
-                    name=provider.replace("_", " ").title(),
-                    on_enter=ExtensionCustomAction(provider,
-                                                   keep_app_open=True)))
+                    name=result.package_name,
+                    on_enter=CopyToClipboardAction(result.package_name)))
 
-        return RenderResultListAction(items)
+
+# def list_providers(self, query):
+#
+#     providers = FLUTTER_PUB_PROVIDERS
+#     if query:
+#         providers = [
+#             provider for provider in providers if query.lower() in provider
+#         ]
+#
+#     items = []
+#
+#     if len(providers) == 0:
+#         return RenderResultListAction([
+#             ExtensionResultItem(
+#                 icon='images/icon.png',
+#                 name="No provider found matching your criteria",
+#                 highlightable=False,
+#                 on_enter=HideWindowAction())
+#         ])
+#
+#     for provider in providers[:20]:
+#         items.append(
+#             ExtensionSmallResultItem(
+#                 icon='images/icon.png',
+#                 name=provider.replace("_", " ").title(),
+#                 on_enter=ExtensionCustomAction(provider,
+#                                                keep_app_open=True)))
+#
+#     return RenderResultListAction(items)
 
 
 class KeywordQueryEventListener(EventListener):
@@ -65,33 +85,33 @@ class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
         """ Handles the event """
-        return extension.list_providers(event.get_argument())
+        return extension.search_params(event.get_argument())
 
 
-class ItemEnterEventListener(EventListener):
-    """ Listener that handles the click on an item """
-
-    def on_event(self, event, extension):
-        """ Handles the event """
-        selected_provider = event.get_data()
-
-        items = []
-
-        movies = get_movies(selected_provider)
-
-        for movie in movies:
-            items.append(
-                ExtensionResultItem(
-                    icon='images/icon.png',
-                    name=movie.movie_name,
-                    highlightable=False,
-                    on_enter=OpenUrlAction(movie.youtube_url),
-                    on_alt_enter=OpenUrlAction(movie.book_url),
-                    description=movie.description,
-                )
-            )
-
-        return RenderResultListAction(items)
+# class ItemEnterEventListener(EventListener):
+#     """ Listener that handles the click on an item """
+#
+#     def on_event(self, event, extension):
+#         """ Handles the event """
+#         selected_provider = event.get_data()
+#
+#         items = []
+#
+#         movies = get_movies(selected_provider)
+#
+#         for movie in movies:
+#             items.append(
+#                 ExtensionResultItem(
+#                     icon='images/icon.png',
+#                     name=movie.movie_name,
+#                     highlightable=False,
+#                     on_enter=OpenUrlAction(movie.youtube_url),
+#                     on_alt_enter=OpenUrlAction(movie.book_url),
+#                     description=movie.description,
+#                 )
+#             )
+#
+#         return RenderResultListAction(items)
 
 
 if __name__ == '__main__':
